fec7a630ff6a6d1a868fb091eb33132908b92bf5
germanlozickyj <germanlozickyj@users.noreply.github.com>
2024-03-23 00:25:07 +0000

    Fix styling

--- config/mercado-pago.php
+++ config/mercado-pago.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    
+
     'access_token' => env('MERCADO_PAGO_ACCESS_TOKEN'),
 
 ];
--- src/Checkout.php
+++ src/Checkout.php
@@ -2,10 +2,10 @@
 
 namespace LaravelMercadoPago\LaravelMercadoPago;
 
+use DateTimeInterface;
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Redirect;
-use DateTimeInterface;
 
 class Checkout implements Responsable
 {
@@ -20,7 +20,7 @@ class Checkout implements Responsable
     private string $product_id;
 
     private array $custom_data = [];
-    
+
     private string $currency_id;
 
     private int $amount;
@@ -29,7 +29,7 @@ class Checkout implements Responsable
 
     private ?DateTimeInterface $expiresAt;
 
-    public function __construct(string $product_id) 
+    public function __construct(string $product_id)
     {
     }
 
@@ -41,89 +41,89 @@ class Checkout implements Responsable
     public function url(): string
     {
         $response = LaravelMercadoPago::api('POST', 'checkout/preferences', [
-                "back_urls"=> $this->getBackUrls(),
-                "differential_pricing"=> [
-                  "id"=> null
+            'back_urls' => $this->getBackUrls(),
+            'differential_pricing' => [
+                'id' => null,
+            ],
+            'expires' => isset($this->expiresAt) ? $this->expiresAt->format(DateTimeInterface::ATOM) : null,
+            'items' => [
+                [
+                    'title' => $this->title,
+                    'description' => $this->description,
+                    'picture_url' => $this->picture_url,
+                    'category_id' => $this->category_id,
+                    'quantity' => $this->quantity,
+                    'currency_id' => $this->currency_id,
+                    'unit_price' => $this->amount,
                 ],
-                "expires"=> isset($this->expiresAt) ? $this->expiresAt->format(DateTimeInterface::ATOM) : null,
-                "items"=> [
-                  [
-                    "title"=> $this->title,
-                    "description"=> $this->description,
-                    "picture_url"=> $this->picture_url,
-                    "category_id"=> $this->category_id,
-                    "quantity"=> $this->quantity,
-                    "currency_id"=> $this->currency_id,
-                    "unit_price"=> $this->amount
-                  ]
+            ],
+            'marketplace_fee' => null,
+            'metadata' => null,
+            'payer' => [
+                'phone' => [
+                    'number' => null,
                 ],
-                "marketplace_fee"=> null,
-                "metadata"=> null,
-                "payer"=> [
-                  "phone"=> [
-                    "number"=> null
-                  ],
-                  "identification"=> [],
-                  "address"=> [
-                    "street_number"=> null
-                  ]
+                'identification' => [],
+                'address' => [
+                    'street_number' => null,
                 ],
-                "payment_methods"=> [
-                  "excluded_payment_methods"=> [
-                    []
-                  ],
-                  "excluded_payment_types"=> [
-                    []
-                  ],
-                  "installments"=> null,
-                  "default_installments"=> null
+            ],
+            'payment_methods' => [
+                'excluded_payment_methods' => [
+                    [],
                 ],
-                "shipments"=> [
-                  "local_pickup"=> false,
-                  "default_shipping_method"=> null,
-                  "free_methods"=> [
+                'excluded_payment_types' => [
+                    [],
+                ],
+                'installments' => null,
+                'default_installments' => null,
+            ],
+            'shipments' => [
+                'local_pickup' => false,
+                'default_shipping_method' => null,
+                'free_methods' => [
                     [
-                      "id"=> null
-                    ]
-                  ],
-                  "cost"=> null,
-                  "free_shipping"=> false,
-                  "receiver_address"=> [
-                    "street_number"=> null
-                  ]
+                        'id' => null,
+                    ],
                 ],
-                "tracks"=> [
-                  []
-                ]
+                'cost' => null,
+                'free_shipping' => false,
+                'receiver_address' => [
+                    'street_number' => null,
+                ],
+            ],
+            'tracks' => [
+                [],
+            ],
         ]);
 
         return $response;
     }
 
-    public function getBackUrls() : array 
+    public function getBackUrls(): array
     {
-      $keys = [
-        'success',
-        'pending',
-        'failed'
-      ];
+        $keys = [
+            'success',
+            'pending',
+            'failed',
+        ];
 
-      $urls = [];
+        $urls = [];
 
-      foreach($keys as $key) {
-        if(isset($this->custom_data["back_{$key}_url"])) {
-          array_push($urls, $this->custom_data["back_{$key}_url"]);
+        foreach ($keys as $key) {
+            if (isset($this->custom_data["back_{$key}_url"])) {
+                array_push($urls, $this->custom_data["back_{$key}_url"]);
+            }
         }
-      }
 
-      return $urls;
+        return $urls;
     }
 
-    public function ExpirationTime() : self
+    public function ExpirationTime(): self
     {
-      return $this;
+        return $this;
     }
- 
+
     public function redirect(): RedirectResponse
     {
         return Redirect::to($this->url(), 303);
@@ -141,7 +141,7 @@ class Checkout implements Responsable
         return $this;
     }
 
-    public function withDescription(string $description) : self
+    public function withDescription(string $description): self
     {
         $this->description = $description;
 
@@ -176,10 +176,10 @@ class Checkout implements Responsable
         return $this;
     }
 
-    public function withQuantity(int $quantity) : self
+    public function withQuantity(int $quantity): self
     {
         $this->quantity = $quantity;
-        
+
         return $this;
     }
 
