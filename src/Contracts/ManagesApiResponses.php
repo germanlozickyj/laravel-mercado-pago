a4d9643df33d06c71f5bd2429d37d49f372deacc
germanlozickyj <germanlozickyj2003@gmail.com>
2024-04-19 22:28:17 -0300

    making interface

---
+++ src/Contracts/ManagesApiResponses.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace LaravelMercadoPago\LaravelMercadoPago\Contracs;
+
+interface ManagesApiResponses {
+    public function HandleStatusCode();
+
+    public function HandleExpection();
+
+    public function HandleResponse();
+
+    public function resolve();
+}
